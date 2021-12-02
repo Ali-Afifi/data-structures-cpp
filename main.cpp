b8ce397568925bb63762c8bd5180dd05da465c8c
Ali-Afifi <ali.m.afify2001@gmail.com>
2021-12-02 07:14:43 +0200

    added queue.hpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
-#include "stack.hpp"
+
 
 int main()
 {
-    Stack stack = Stack(15);
-
-    std::cout << stack.pop() << std::endl;
-
-    stack.push(4);
-    stack.push(7);
-    stack.push(8);
-    stack.push(41);
-    stack.push(41);
-    stack.push(41);
-    stack.push(41);
-    stack.push(41);
-    stack.push(41);
-    stack.push(41);
-
-    printf("%i\n", stack.push(4));
-    printf("%i\n", stack.peek());
-    
-    stack.print();
+  
     return 0;
 }
