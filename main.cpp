eccb97844bbf5729d833a35c59d1523c9066c3fd
Ali-Afifi <ali.m.afify2001@gmail.com>
2021-12-02 04:38:49 +0200

    added stack.hpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-#include "linked-list.hpp"
+#include "stack.hpp"
 
 int main()
 {
+    Stack stack = Stack(15);
+
+    std::cout << stack.pop() << std::endl;
+
+    stack.push(4);
+    stack.push(7);
+    stack.push(8);
+    stack.push(41);
+    stack.push(41);
+    stack.push(41);
+    stack.push(41);
+    stack.push(41);
+    stack.push(41);
+    stack.push(41);
+
+    printf("%i\n", stack.push(4));
+    printf("%i\n", stack.peek());
+    
+    stack.print();
     return 0;
 }
